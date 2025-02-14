33c9f7512146ea141a0aed01bf31989f2053b7a0
ruzica-glavas <ruzi99@hotmail.it>
2025-02-14 14:35:35 +0100

    add: move notFound and errorsHandler at the end of all app.use

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const postsRouter = require('./routers/posts.js'); //l'import della cartella rou
 
 //Importazione dei middlewares
 const errorsHandler = require ("./middlewares/errorsHandler.js")
-const notFound = require ("./middlewares/notFound.js")
+const notFound = require ("./middlewares/notFound.js");
 
-//Inserire la lettura dei middleware
-app.use(errorsHandler);
-app.use (notFound)
 
 //Inserire la lettura della cartella public
 app.use(express.static('public'));
@@ -18,15 +15,16 @@ app.use(express.static('public'));
 //body-parser
 app.use(express.json());
 
-
 //Indica a express una nuova rotta, in questo caso il prefisso sarà "/posts" e il router postsRouter
 app.use("/posts", postsRouter);
 
-
-
+//Inserire la lettura dei middleware
+app.use(errorsHandler);
+app.use (notFound);
 
 
 //Attiva il server http:localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
